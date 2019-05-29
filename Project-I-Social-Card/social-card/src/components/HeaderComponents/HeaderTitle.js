ab0e414fa37bef96cecad7f594d29dafb73fbade
Matt Hagner <web@matthagner.com>
2019-05-28 22:26:54 -0500

    Complete structure of HeaderTitle

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+export default function HeaderTitle() {
+  return (
+    <div className="title--container">
+      <p className="title">Lambda School</p>
+      <p className="title--detail">@LambdaSchool 26 jan</p>
+    </div>
+  );
+}
