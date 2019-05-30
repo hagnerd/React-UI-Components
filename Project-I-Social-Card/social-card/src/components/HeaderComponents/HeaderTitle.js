de5a360643d3a1f9b86a7bd7f0ba4418aad80904
Matt Hagner <web@matthagner.com>
2019-05-29 19:50:11 -0500

    Tweak HeaderContainer styles

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -5,7 +5,7 @@ export default function HeaderTitle() {
   return (
     <div className="title--container">
       <p className="title">Lambda School</p>
-      <p className="title--detail">@LambdaSchool 26 jan</p>
+      <p className="title--detail">@LambdaSchool &#183; 26 jan</p>
     </div>
   );
 }
