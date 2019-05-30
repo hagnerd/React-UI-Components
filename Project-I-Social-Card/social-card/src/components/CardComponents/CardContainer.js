71dbd1891fc77a299298f21983fbf74db4fb38ba
Matt Hagner <web@matthagner.com>
2019-05-29 20:06:23 -0500

    Tweak Card styles

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CardBanner from "./CardBanner";
+import CardContent from "./CardContent";
 import "./Card.css";
 
-export default function CardContent() {
+export default function CardContainer() {
   return (
     <div className="card--container">
       <CardBanner />
+      <CardContent />
     </div>
   );
 }
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from "react";
 import "./Card.css";
+
+export default function CardContent() {
+  return (
+    <div className="card--content">
+      <h4>Get started with React</h4>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application.
+      </p>
+      <a href="reactjs.org" target="_blank">
+        reactjs.org
+      </a>
+    </div>
+  );
+}
