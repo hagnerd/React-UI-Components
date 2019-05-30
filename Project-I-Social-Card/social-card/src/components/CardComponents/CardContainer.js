f6a101a0ac0c2798b4387d560d800957c4fd58ba
Matt Hagner <web@matthagner.com>
2019-05-29 19:20:01 -0500

    Fix error

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,11 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import CardBanner from "./CardBanner";
+import "./Card.css";
+
+export default function CardContent() {
+  return (
+    <div>
+      <CardBanner />
+    </div>
+  );
+}
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,11 +1,2 @@
 import React from "react";
 import "./Card.css";
-import CardBanner from "./CardBanner";
-
-export default function CardContent() {
-  return (
-    <div>
-      <CardBanner />
-    </div>
-  );
-}
