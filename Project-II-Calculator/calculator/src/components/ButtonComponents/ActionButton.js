240ae914190f3a981c416f6940a344a22aeb2064
Matt Hagner <web@matthagner.com>
2019-06-01 15:42:23 -0500

    Update ActionButton to handle click

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,15 +1,28 @@
 import React from "react";
-import { string as propString } from "prop-types";
+import { string as propString, func } from "prop-types";
 import "./Button.css";
 
 export default function ActionButton({
   text,
-  actionStyle = "btn text-l bg-carmine text-color-white weight-light"
+  actionStyle = "btn text-l bg-carmine text-color-white weight-light",
+  handleClick,
+  type = "operator",
+  keyAlt = text
 }) {
-  return <button className={actionStyle}>{text}</button>;
+  return (
+    <button
+      className={actionStyle}
+      onClick={() => handleClick({ type, key: keyAlt })}
+    >
+      {text}
+    </button>
+  );
 }
 
 ActionButton.propTypes = {
   text: propString.isRequired,
+  handleClick: func.isRequired,
+  type: propString,
+  keyAlt: propString,
   actionStyle: propString
 };
