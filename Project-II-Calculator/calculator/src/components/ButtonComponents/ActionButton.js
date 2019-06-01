f8e908c9290519666bbe7eceecaee7bcbd751926
Matt Hagner <web@matthagner.com>
2019-06-01 11:59:49 -0500

    Add ActionButton component

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import { string as propString } from "prop-types";
+import "./Button.css";
+
+export default function ActionButton({ text, actionStyle }) {
+  return <button className={actionStyle}>{text}</button>;
+}
+
+ActionButton.propTypes = {
+  text: propString(),
+  actionStyle: propString()
+};
