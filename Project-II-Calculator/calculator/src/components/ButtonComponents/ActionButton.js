c685b890cc2da03682bfe44b111feb992d8c5e83
Matt Hagner <web@matthagner.com>
2019-06-01 12:32:50 -0500

    Fix proptypes for ActionButton and NumberButton

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -2,11 +2,11 @@ import React from "react";
 import { string as propString } from "prop-types";
 import "./Button.css";
 
-export default function ActionButton({ text, actionStyle }) {
+export default function ActionButton({ text, actionStyle = "" }) {
   return <button className={actionStyle}>{text}</button>;
 }
 
 ActionButton.propTypes = {
-  text: propString(),
-  actionStyle: propString()
+  text: propString.isRequired,
+  actionStyle: propString
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,12 +1,12 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { string as propString } from "prop-types";
 import "./Button.css";
 
-export default function NumberButton({ text, buttonStyle }) {
+export default function NumberButton({ text, buttonStyle = "" }) {
   return <button className={buttonStyle}>{text}</button>;
 }
 
 NumberButton.propTypes = {
-  text: PropTypes.string(),
-  buttonStyle: PropTypes.string()
+  text: propString.isRequired,
+  buttonStyle: propString
 };
