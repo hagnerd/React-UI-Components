49d067602f42b7c481d73cf4c0aed883d110d1d0
Matt Hagner <web@matthagner.com>
2019-06-01 15:42:41 -0500

    Update NumberButton to handle click

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { string as propString } from "prop-types";
+import { string as propString, func } from "prop-types";
 import "./Button.css";
 
-export default function NumberButton({ text, buttonStyle = "btn" }) {
-  return <button className={buttonStyle}>{text}</button>;
+export default function NumberButton({
+  text,
+  buttonStyle = "btn",
+  handleClick
+}) {
+  return (
+    <button
+      className={buttonStyle}
+      onClick={() => handleClick({ type: "number", key: text })}
+    >
+      {text}
+    </button>
+  );
 }
 
 NumberButton.propTypes = {
   text: propString.isRequired,
+  handleClick: func.isRequired,
   buttonStyle: propString
 };
