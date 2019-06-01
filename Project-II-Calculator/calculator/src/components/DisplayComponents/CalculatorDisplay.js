1f1fa242af2e22b92ab1f88e4b419819244b119c
Matt Hagner <web@matthagner.com>
2019-06-01 12:02:08 -0500

    Add CalculatorDisplay component

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import { string as propString } from "prop-types";
+import "./Display.css";
+
+export default function CalculatorDisplay({ text, className }) {
+  return <h2 className={className}>{text}</h2>;
+}
+
+CalculatorDisplay.propTypes = {
+  text: propString(),
+  className: propString()
+};
